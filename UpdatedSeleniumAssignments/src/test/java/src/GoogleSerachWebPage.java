1dda516daa913ee26c0b536fa0db30d5f2613d4a
acer <acer@DESKTOP-S2I7VH9>
2021-03-03 21:26:25 +0530

    second commit

---
+++ UpdatedSeleniumAssignments/src/test/java/src/GoogleSerachWebPage.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSerachWebPage {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\acer\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("http://www.google.com");
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("java");
+		
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='sbl1']"));
+		
+		System.out.println("Total number of suggestions "+ list.size());
+		
+		for (int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i).getText());
+			if (list.get(i).getText().contains("javascript tutorial")) {
+			list.get(i).click();
+			break;
+			}
+		}
+		driver.close();
+
+			
+	}
+
+}
